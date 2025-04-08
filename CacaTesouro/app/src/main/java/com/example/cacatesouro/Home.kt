d5ec6fcf8ed0824ef5829b90dde95199dbfe7d2d
William Espindola Cardoso <williamcardoso_sc@hotmail.com>
2025-04-08 01:20:42 -0300

    Exercicio 3

---
+++ CacaTesouro/app/src/main/java/com/example/cacatesouro/Home.kt
@@ -0,0 +1,27 @@
+package com.example.cacatesouro
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Home(onStart: () -> Unit) {
+    Column (
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { onStart() }) {
+            Text("Iniciar Caça ao Tesouro",
+                fontSize = 22.sp,
+                fontWeight = FontWeight.Bold)
+        }
+    }
+}
---
+++ CacaTesouro/app/src/main/java/com/example/cacatesouro/Pista3.kt
@@ -0,0 +1,42 @@
+package com.example.cacatesouro
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+    fun Pista3(onBack: () -> Unit, onNext: () -> Unit) {
+        Column (
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Pista 3",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Bold)
+
+            Spacer(modifier = Modifier.height(18.dp))
+
+            Button(onClick = { onBack() }) {
+                Text("Voltar a Segunda Pista",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold)
+            }
+            Button(onClick = { onNext() }) {
+                Text("Respoder",
+                    fontSize = 16.sp,
+                    fontWeight = FontWeight.Bold)
+            }
+        }
+    }
+
